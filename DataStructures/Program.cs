2a61c01ad2ae3efb4f089ae7a5d2933468ea25c5
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-12-02 21:11:21 -0800

    Selection Sort

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DataStructures.BinaryTree;
 using DataStructures.SinglyLinkedList;
+using DataStructures.SortingAlgorithms;
 
 namespace DataStructures
 {
@@ -8,7 +9,18 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-
+            var ss = new SelectionSort();
+            var arr = new int[] { 3, 2, 6, 8, 1, 2 };
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}, ");
+            }
+            var newArr = ss.Sort(arr);
+            Console.WriteLine();
+            foreach (var item in newArr)
+            {
+                Console.Write($"{item}, ");
+            }
         }
     }
 }
---
+++ DataStructures/Sorting Algorithms/SelectionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DataStructures.SortingAlgorithms
+{
+    public class SelectionSort
+    {
+        public int [] Sort(int[] arr)
+        {
+            int temp;
+            int smallest;
+            int index = 0;
+            var len = arr.Length;
+            for (int i = 0; i < len; i++)
+            {
+                smallest = arr[i];
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (arr[j] <= smallest)
+                    {
+                        smallest = arr[j];
+                        index = j;
+                    }
+                }
+
+                temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+            }
+            return arr;
+        }
+    }
+}
