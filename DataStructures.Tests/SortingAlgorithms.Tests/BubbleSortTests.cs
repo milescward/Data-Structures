5baebb87b17231e86a2096b5e2ec1d63733e24fb
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-12-02 21:18:41 -0800

    BubbleSort test

---
+++ DataStructures.Tests/SortingAlgorithms.Tests/BubbleSortTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataStructures.SortingAlgorithms;
+using Xunit;
+
+namespace DataStructures.Tests.SortingAlgorithms.Tests
+{
+    public class BubbleSortTests
+    {
+        [Fact]
+        public void BubbleSortTest()
+        {
+            //Arrange
+            var bs = new BubbleSort();
+            var arr = new int[] { 3, 2, 6, 8, 1, 2 };
+            //Act
+            var expected = new int[] { 1, 2, 2, 3, 6, 8 };
+            var actual = bs.Sort(arr);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
