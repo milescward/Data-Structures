39768519c56fbf6bc607af3a7ba153ecdf0c85b5
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-12-02 21:23:38 -0800

    Insertion and Selection Sort tests

---
+++ DataStructures.Tests/SortingAlgorithms.Tests/InsertionSortTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataStructures.SortingAlgorithms;
+using Xunit;
+
+namespace DataStructures.Tests.SortingAlgorithms.Tests
+{
+    public class InsertionSortTests
+    {
+        [Fact]
+        public void SelectionSortTest()
+        {
+            //Arrange
+            var IS = new InsertionSort();
+            var arr = new int[] { 3, 2, 6, 8, 1, 2 };
+            //Act
+            var expected = new int[] { 1, 2, 2, 3, 6, 8 };
+            var actual = IS.Sort(arr);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ DataStructures.Tests/SortingAlgorithms.Tests/SelectionSortTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataStructures.SortingAlgorithms;
+using Xunit;
+
+namespace DataStructures.Tests.SortingAlgorithms.Tests
+{
+    public class SelectionSortTests
+    {
+        [Fact]
+        public void SelectionSortTest()
+        {
+            //Arrange
+            var ss = new SelectionSort();
+            var arr = new int[] { 3, 2, 6, 8, 1, 2 };
+            //Act
+            var expected = new int[] { 1, 2, 2, 3, 6, 8 };
+            var actual = ss.Sort(arr);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
